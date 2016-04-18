789e1568318a53cbe7e1fa0f55283023acb8cb43
Julian Feliciano <julsfelic@gmail.com>
2016-04-18 10:03:21 -0600

    Finishing up the first feature test
    In the process of fleshing out the first feature test now that we have
    our service poro working and know what the endpoint gives us

--- spec/features/user_can_search_for_ten_closests_stations_spec.rb
+++ spec/features/user_can_search_for_ten_closests_stations_spec.rb
@@ -8,8 +8,27 @@ RSpec.feature "User can search for ten closests stations" do
 
   expect(current_path).to eq("/search?zip=80203")
 
-  # Build out the Service and come back to test
-  # Then I should see a list of the 10 closest stations within 6 miles sorted by distance
-  # And the stations should be limited to Electric and Propane
+  list = []
+  list = find(".stations").all("li")
+  expect(list.size).to eq(10)
+  expect(list[0]).to have_content("ELEC")
+  expect(list[0]).to have_content("800 Acoma St")
+  expect(list[0]).to have_content("0.3117 mi")
+  expect(list[0]).to have_content("UDR")
+  expect(list[9]).to have_content("ELEC")
+  expect(list[9]).to have_content("1286-1296 Stout St")
+  expect(list[9]).to have_content("1.13847 mi")
+  expect(list[0]).to have_content("PUBLIC STATIONS")
+
+  list.each do |station|
+    expect(station).to_not have_content("BD")
+    expect(station).to_not have_content("CNG")
+    expect(station).to_not have_content("E85")
+    expect(station).to_not have_content("HY")
+    expect(station).to_not have_content("LNG")
+  end
+
+
+
   # And for each of the stations I should see Name, Address, Fuel Types, Distance, and Access Times
 end
