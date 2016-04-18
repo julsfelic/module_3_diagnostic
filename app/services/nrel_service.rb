5cf23f34e5d859997978f9366a90df90f5814b16
Julian Feliciano <julsfelic@gmail.com>
2016-04-18 09:46:50 -0600

    Pass nrel service spec
    Just passed the test for nrel service spec

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -1,11 +1,11 @@
 class NrelService
   def initialize
-    @_connection = faraday.new("http://developer.nrel.gov/api")
+    @_connection = Faraday.new("http://developer.nrel.gov")
     connection.params["api_key"] = ENV["NREL_API_KEY"]
   end
 
   def nearest_alt_fuel_station(params)
-    parse(connection.get("/alt-fuel-stations/v1/nearest.json"), params)
+    parse(connection.get("/api/alt-fuel-stations/v1/nearest.json", params))
   end
 
   private
@@ -15,6 +15,6 @@ class NrelService
     end
 
     def parse(response)
-      JSON.parse(response.body)
+      (JSON.parse(response.body)).deep_symbolize_keys
     end
 end
